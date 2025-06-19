199544ba7b05c9af1e28128b6fa9f0ed2ad26029
Lemon-Juiced <47193272+Lemon-Juiced@users.noreply.github.com>
2025-06-19 18:06:41 -0400

    Started work on Lemix script will be used for DTar and aptb

---
+++ lemix.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Elevate privileges if not already running as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script requires root privileges. Please run it with sudo."
+    exit 1
+fi
+
+# wget the `aptb` and `DTar` files
+echo "Downloading aptb and DTar files..."
+# To Do: wget aptb
+wget https://github.com/Lemon-Juiced/DTar/releases/download/v0.0.3/DTar.tar.gz
+
+# Unzip the DTar file
+echo "Unzipping DTar file..."
+tar -xzf DTar.tar.gz
+rm DTar.tar.gz
+
+# Extract DTar.tar.gz and organize contents
+echo "Extracting DTar.tar.gz..."
+tar -xzf DTar.tar.gz
+mv linux/man/* /usr/local/share/man/man1/
+mv linux/* /usr/local/bin/
+# Update man database
+mandb
+
+# Make all scripts executable
+echo "Making scripts executable..."
+chmod +x /usr/local/bin/*
+
+echo "Installation completed successfully."
